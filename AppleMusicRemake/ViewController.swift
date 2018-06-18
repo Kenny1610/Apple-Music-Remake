445906c1ebf3dc5ee07279e285c3bcac1eafaf69
Kendall <kendallmccaskill@gmail.com>
2018-06-17 18:56:53 -0700

    Initial Commit

---
+++ AppleMusicRemake/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppleMusicRemake
+//
+//  Created by Kendall McCaskill on 6/17/18.
+//  Copyright © 2018 Kendall McCaskill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
